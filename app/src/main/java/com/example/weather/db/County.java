7d7aed9e6409d8b4654ced24027cadc118f78c74
leiteorz <2442075815@qq.com>
2022-04-04 21:30:43 +0800

    完成遍历省市县三级列表的功能.

--- app/src/main/java/com/example/weather/db/County.java
+++ app/src/main/java/com/example/weather/db/County.java
@@ -6,7 +6,7 @@ public class County extends DataSupport {
     private int id;
     private String countyName;
     private String weatherId;
-    private String cityId;
+    private int cityId;
     public int getId() {
         return id;
     }
@@ -31,11 +31,11 @@ public class County extends DataSupport {
         this.weatherId = weatherId;
     }
 
-    public String getCityId() {
+    public int getCityId() {
         return cityId;
     }
 
-    public void setCityId(String cityId) {
+    public void setCityId(int cityId) {
         this.cityId = cityId;
     }
 
